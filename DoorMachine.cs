47c79a03a7e24729336fa38ac06f927ce0dd6a34
Yusak Abiyyu <yusakabiyyu@gmail.com>
2026-03-16 01:28:14 +0700

    Add DoorMachine class

---
+++ DoorMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class DoorMachine
+{
+    enum State { Terkunci, Terbuka }
+
+    private State state;
+
+    public DoorMachine()
+    {
+        state = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void BukaPintu()
+    {
+        if (state == State.Terkunci)
+        {
+            state = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+
+    public void KunciPintu()
+    {
+        if (state == State.Terbuka)
+        {
+            state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+    }
+}
